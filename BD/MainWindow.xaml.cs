f33b2e9f08b0e0d33ddadc4aded2105d572733fd
Koserwin <kosinaer@email.cz>
2024-10-14 20:18:45 +0200

    Levely Done

--- BD/MainWindow.xaml.cs
+++ BD/MainWindow.xaml.cs
@@ -24,10 +24,57 @@ namespace BD
             {'z', 'h','h','h','h','h','h','h','h','z'},
             {'z', 'h','d','h','z','h','h','h','h','z'},
             {'z', 'd','z','P','z','z','h','z','d','z'},
-            {'z', 'z','z','z','D','h','h','z','z','z'},
+            {'z', 'z','z','z','D','h','h','h','z','z'},
             {'z', 'd','k','k','k','h','z','k','d','z'},
             {'z', 'd','h','h','h','h','h','h','d','z'},
             {'z', 'z','z','z','z','z','z','z','z','z'},
+        };
+
+        char[,] level2 =
+        {
+            {'z', 'z','z','z','z','z','z','z','z','z','z','z','z','z','z','z','z'},
+            {'z', 'h','d','h','z','d','d','z','z','z','z','z','k','k','z','z','z'},
+            {'z', 'h','z','h','z','d','d','z','z','z','z','z','d','d','d','z','z'},
+            {'z', 'P','k','h','z','k','h','h','k','k','z','d','h','h','h','z','z'},
+            {'z', 'z','k','d','z','h','h','h','h','d','h','z','d','d','h','z','z'},
+            {'z', 'z','d','d','z','h','z','h','z','z','z','z','z','z','h','z','z'},
+            {'z', 'k','h','d','z','h','z','h','h','d','z','z','z','z','h','z','z'},
+            {'D', 'h','h','h','z','h','z','d','d','d','z','z','z','z','h','z','z'},
+            {'z', 'd','z','h','h','h','z','d','d','z','z','z','z','z','h','z','z'},
+            {'z', 'z','z','z','h','z','z','z','z','h','k','d','z','z','e','z','z'},
+            {'z', 'z','k','k','h','z','z','z','z','d','d','d','z','z','h','h','z'},
+            {'z', 'h','h','h','h','h','z','z','z','h','z','z','z','z','h','h','z'},
+            {'z', 'd','h','h','h','h','h','h','e','h','h','h','h','h','h','z','z'},
+            {'z', 'd','d','h','z','z','z','k','h','d','z','z','z','h','h','z','z'},
+            {'z', 'z','z','d','z','z','z','d','d','d','z','z','d','d','d','z','z'},
+            {'z', 'z','z','z','z','z','z','z','z','z','z','z','z','z','z','z','z'},
+        };
+
+        char[,] level3 =
+        {
+            {'z', 'z','z','z','z','z','z','z','z','z','z','z','z'},
+            {'z', 'z','z','z','z','k','z','z','d','d','z','P','z'},
+            {'z', 'd','d','z','h','d','z','h','h','h','h','h','z'},
+            {'z', 'd','h','h','h','h','h','h','e','h','h','h','z'},
+            {'z', 'd','h','h','e','h','h','h','h','h','h','d','z'},
+            {'z', 'h','h','h','z','z','z','h','h','z','d','z','z'},
+            {'z', 'z','z','h','d','z','z','h','z','k','k','z','z'},
+            {'z', 'z','h','d','d','z','z','h','h','h','d','z','z'},
+            {'z', 'z','h','z','z','z','z','h','e','h','d','z','z'},
+            {'z', 'z','h','d','z','d','z','h','h','h','z','z','z'},
+            {'z', 'z','h','z','z','h','z','z','d','d','z','z','z'},
+            {'z', 'h','h','z','z','d','z','z','z','d','z','k','z'},
+            {'z', 'z','e','z','z','d','z','z','z','h','h','d','z'},
+            {'z', 'k','h','h','h','e','h','h','h','h','d','d','z'},
+            {'z', 'd','d','d','z','z','z','h','z','z','z','d','z'},
+            {'z', 'h','z','z','z','z','k','d','z','z','z','h','z'},
+            {'z', 'h','k','k','z','z','k','h','z','z','k','h','z'},
+            {'z', 'h','h','d','z','d','h','h','z','d','h','h','z'},
+            {'z', 'h','h','z','z','d','h','h','z','z','h','h','z'},
+            {'D', 'd','d','z','z','z','d','d','z','z','z','z','z'},
+            {'z', 'h','z','z','z','z','d','z','z','z','z','z','z'},
+            {'z', 'z','z','z','z','z','z','z','z','z','z','z','z'},
+
         };
         public MainWindow()
         {
@@ -35,11 +82,12 @@ namespace BD
 
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void ShowLevel(char[,] SelectedLevel)
         {
+            char[,] level = SelectedLevel;
             Setup.plocha = hra;
-            int radek = level1.GetLength(1);
-            int sloupec = level1.GetLength(0);
+            int radek = level.GetLength(1);
+            int sloupec = level.GetLength(0);
             for (int i = 0; i < radek; i++)
             {
                 ColumnDefinition c1 = new ColumnDefinition();
@@ -60,7 +108,7 @@ namespace BD
                 for (int r = 0; r < radek; r++)
                 {
                     Pole p;
-                    switch (level1[s, r])
+                    switch (level[s, r])
                     {
                         case 'z':
                             p = new Wall(r,s);
@@ -95,7 +143,30 @@ namespace BD
             }
         }
 
-        
+        private void level1_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            hra.ColumnDefinitions.Clear();
+            hra.RowDefinitions.Clear();
+            ShowLevel(level1);
+        }
 
+        private void level2_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            hra.ColumnDefinitions.Clear();
+            hra.RowDefinitions.Clear();
+            ShowLevel(level2);
+        }
+
+        private void level3_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            hra.ColumnDefinitions.Clear();
+            hra.RowDefinitions.Clear();
+            ShowLevel(level3);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowLevel(level1);
+        }
     }
 }
